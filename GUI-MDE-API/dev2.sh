a7f4a1912581d448d971704e1c0216afb02bd85b
Kijo <120234772+LearningKijo@users.noreply.github.com>
2023-12-18 11:26:42 +0900

    Update dev2.sh

--- GUI-MDE-API/dev2.sh
+++ GUI-MDE-API/dev2.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
+# Specify the name of the ZIP file
+zip_file_name="MicrosoftAuthenticator.app.zip"
+
+# Specify the full path to the ZIP file
+zip_file_path="/Users/luiza/Downloads/$zip_file_name"
+
+# Specify the name of the extracted application
+app_name="MicrosoftAuthenticator.app"
+
 # Specify the full path to the extracted application
 app_path="/Users/luiza/Downloads/$app_name"
 
 # Specify the URL to download the ZIP file
-download_url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip"
+download_url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/$zip_file_name"
 
 # Download the ZIP file
 echo "Downloading the ZIP file..."
@@ -12,6 +23,19 @@ curl -L -o "$zip_file_path" "$download_url"
 if [ $? -eq 0 ]; then
     echo "Download completed successfully."
 
+    # Check extended attributes for MicrosoftAuthenticator.app
+    echo "Checking extended attributes for $app_name..."
+    xattr_result=$(xattr "$zip_file_path")
+
+    # Check if "com.apple.quarantine" is present in the extended attributes
+    if [[ $xattr_result == *"com.apple.quarantine"* ]]; then
+        echo "Removing 'com.apple.quarantine' attribute..."
+        xattr -d com.apple.quarantine "$zip_file_path"
+        echo "Attribute removed."
+    else
+        echo "No 'com.apple.quarantine' attribute found."
+    fi
+
     # Unzip the file
     echo "Extracting the ZIP file..."
     unzip -o "$zip_file_path" -d "/Users/luiza/Downloads/"
@@ -19,19 +43,6 @@ if [ $? -eq 0 ]; then
     # Check if the extracted app exists
     if [ -e "$app_path" ]; then
         echo "Extraction completed successfully."
-
-        # Check extended attributes for MicrosoftAuthenticator.app
-        echo "Checking extended attributes for $app_name..."
-        xattr_result=$(xattr "$app_path")
-
-        # Check if "com.apple.quarantine" is present in the extended attributes
-        if [[ $xattr_result == *"com.apple.quarantine"* ]]; then
-            echo "Removing 'com.apple.quarantine' attribute..."
-            xattr -d com.apple.quarantine "$app_path"
-            echo "Attribute removed."
-        else
-            echo "No 'com.apple.quarantine' attribute found."
-        fi
 
         # Run the unzipped app 
         open "$app_path"
