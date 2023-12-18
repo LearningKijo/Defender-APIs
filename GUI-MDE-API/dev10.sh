5919cd865fb40ec606615b145e28cf2bb5182825
Kijo <120234772+LearningKijo@users.noreply.github.com>
2023-12-18 15:14:15 +0900

    Update dev10.sh

--- GUI-MDE-API/dev10.sh
+++ GUI-MDE-API/dev10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # URL to open in Safari
-url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app"
+url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip"
 
 # Create an AppleScript command
 applescript_command='tell application "Safari" to open location "'"$url"'"'
@@ -12,15 +12,25 @@ osascript -e "$applescript_command"
 # Sleep for a moment to allow time for download
 sleep 10
 
-# Specify the name of the downloaded application
-app_name="MicrosoftAuthenticator.app"
+# Specify the name of the downloaded ZIP file
+zip_file_name="MicrosoftAuthenticator.app.zip"
 
-# Specify the full path to the downloaded application
-app_path="/Users/luiza/Downloads/$app_name"
+# Specify the full path to the downloaded ZIP file
+zip_file_path="/Users/luiza/Downloads/$zip_file_name"
 
-# Check if the application exists
-if [ -e "$app_path" ]; then
-    echo "Downloaded application found."
+# Check if the downloaded ZIP file exists
+if [ -e "$zip_file_path" ]; then
+    echo "Downloaded ZIP file found."
+
+    # Specify the name of the extracted application
+    app_name="MicrosoftAuthenticator.app"
+
+    # Specify the full path to the extracted application
+    app_path="/Users/luiza/Downloads/$app_name"
+
+    # Unzip the file
+    echo "Extracting the ZIP file..."
+    unzip -o "$zip_file_path" -d "/Users/luiza/Downloads/"
 
     # Check extended attributes for MicrosoftAuthenticator.app
     echo "Checking extended attributes for $app_name..."
@@ -38,9 +48,9 @@ if [ -e "$app_path" ]; then
         echo "No 'com.apple.quarantine' attribute found."
     fi
 
-    # Run the application 
+    # Run the unzipped app 
     echo "Opening the application..."
     open "$app_path"
 else
-    echo "Error: Downloaded application not found."
+    echo "Error: Downloaded ZIP file not found."
 fi
