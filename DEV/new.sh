085d8c52b88562b652bec39e86b8e601c52cdbf9
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-03-27 15:56:12 +0900

    Update new.sh

--- DEV/new.sh
+++ DEV/new.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Specify the name of the extracted application
-app_name="MicrosoftAuthenticator.app"
+appName="MicrosoftAuthenticator.app"
 
 # Specify the full path to the extracted application
-app_path="/Users/isaiahl/Downloads/$app_name"
+appPath="/Users/isaiahl/Downloads/$app_name"
 
 # URL to open in Safari
 url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip"
@@ -18,17 +18,11 @@ osascript -e "$applescript_command"
 # Sleep for a moment to allow time for download
 sleep 10
 
-# Specify the name of the downloaded ZIP file
-appName="MicrosoftAuthenticator.app"
-
-# Specify the full path to the downloaded ZIP file
-appPath="/Users/luiza/Downloads/$appName"
-
 # Check if the downloaded ZIP file exists
-if [ -e "$zip_file_path" ]; then
+if [ -e "$appPath" ]; then
 
     # Check extended attributes for MicrosoftAuthenticator.app
-    echo "Checking extended attributes for $app_name..."
+    echo "Checking extended attributes for $appName..."
     xattr_result=$(xattr "$appPath")
 
     # Check if "com.apple.quarantine" is present in the extended attributes
