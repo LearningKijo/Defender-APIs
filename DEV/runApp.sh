424bcaf7e5e1c0630137a787fc7a8c6388d15810
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-03-27 15:10:54 +0900

    Update runApp.sh

--- DEV/runApp.sh
+++ DEV/runApp.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-# URL to open in Safari
-url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip"
-
-# Specify the download path
-download_path="/Users/isaiahl/Library/MicrosoftAuthenticator.app"
-
-# Create an AppleScript command
-applescript_command='tell application "Safari" to download "'"$url"'" to POSIX file "'"$download_path"'"'
-
-# Run the AppleScript command
-osascript -e "$applescript_command"
-
-
 # Set the path where you want to download the file
 download_path="/Users/isaiahl/Library/MicrosoftAuthenticator.app"
 
 # Use curl to download the file to the specified path
 curl -o "$download_path" -L https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip
+
+# Sleep for a moment to allow time for download
+sleep 10
+
+# Specify the name of the downloaded ZIP file
+appName="MicrosoftAuthenticator.app"
+
+# Specify the full path to the downloaded ZIP file
+appPath="/Users/isaiahl/Library/$appName"
+
+# Check if the downloaded ZIP file exists
+if [ -e "$appPath" ]; then
+
+    # Check extended attributes for MicrosoftAuthenticator.app
+    echo "[1] GateKeeper check: $appName"
+    xattr_result=$(xattr "$appPath")
+
+    # Check if "com.apple.quarantine" is present in the extended attributes
+    if [[ $xattr_result == *"com.apple.quarantine"* ]]; then
+        echo "  [+] 'com.apple.quarantine' attribute is present."
+        
+        # Removing 'com.apple.quarantine' attribute
+        echo "  [+] Removing 'com.apple.quarantine' attribute..."
+        xattr -d com.apple.quarantine "$appPath"
+        xattr "$appPath"
+        echo "  [+] Attribute removed."
+    else
+        echo "No 'com.apple.quarantine' attribute found."
+    fi
+
+    # Run the unzipped app 
+    echo "[2] App execution : $appName"
+    open "$appPath"
+else
+    echo "Error: Downloaded ZIP file not found."
+fi
