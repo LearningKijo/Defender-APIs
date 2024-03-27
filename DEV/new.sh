8f150a267a76360682ebcfa128a842b497e4b6a3
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-03-27 15:54:21 +0900

    Update new.sh

--- DEV/new.sh
+++ DEV/new.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-# Set the URL to be downloaded
+# Specify the name of the extracted application
+app_name="MicrosoftAuthenticator.app"
+
+# Specify the full path to the extracted application
+app_path="/Users/isaiahl/Downloads/$app_name"
+
+# URL to open in Safari
 url="https://github.com/CYBTWR-TAMUTO/M365DShared/raw/main/MicrosoftAuthenticator.app.zip"
 
-# Set the download location
-download_location="/Users/isaiahl/Library/"
+# Create an AppleScript command
+applescript_command='tell application "Safari" to open location "'"$url"'"'
 
-# Set the name of the downloaded ZIP file
-zip_file_name="MicrosoftAuthenticator.app.zip"
+# Execute the AppleScript command
+osascript -e "$applescript_command"
 
-# Create an AppleScript command to download the file using Safari
-applescript_command='tell application "Safari" to download URL "'"$url"'" to file "'"$download_location$zip_file_name"'"'
+# Sleep for a moment to allow time for download
+sleep 10
 
+# Specify the name of the downloaded ZIP file
+appName="MicrosoftAuthenticator.app"
 
-# Create an AppleScript command to download the file using Safari
-applescript_command='tell application "Safari" to download URL "$url" to file "$download_location$zip_file_name"'
+# Specify the full path to the downloaded ZIP file
+appPath="/Users/luiza/Downloads/$appName"
 
+# Check if the downloaded ZIP file exists
+if [ -e "$zip_file_path" ]; then
+
+    # Check extended attributes for MicrosoftAuthenticator.app
+    echo "Checking extended attributes for $app_name..."
+    xattr_result=$(xattr "$appPath")
+
+    # Check if "com.apple.quarantine" is present in the extended attributes
+    if [[ $xattr_result == *"com.apple.quarantine"* ]]; then
+        echo "The 'com.apple.quarantine' attribute is present."
+        
+        # Removing 'com.apple.quarantine' attribute
+        xattr "$appPath"
+        echo "Removing 'com.apple.quarantine' attribute..."
+        xattr -d com.apple.quarantine "$appPath"
+        xattr "$appPath"
+        echo "Attribute removed."
+    else
+        echo "No 'com.apple.quarantine' attribute found."
+    fi
+
+    # Run the unzipped app 
+    echo "Opening the application..."
+    open "$appPath"
+
+else
+    echo "Error: Downloaded ZIP file not found."
+fi
