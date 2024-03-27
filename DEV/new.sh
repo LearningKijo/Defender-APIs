a086848fafd92f05e799d02a35e9e9f33935c02d
Kijo Ninja <120234772+LearningKijo@users.noreply.github.com>
2024-03-27 15:40:51 +0900

    Update new.sh

--- DEV/new.sh
+++ DEV/new.sh
@@ -12,5 +12,7 @@ zip_file_name="MicrosoftAuthenticator.app.zip"
 # Create an AppleScript command to download the file using Safari
 applescript_command='tell application "Safari" to download URL "'"$url"'" to file "'"$download_location$zip_file_name"'"'
 
-# Execute the AppleScript command
-osascript -e "$applescript_command"
+
+# Create an AppleScript command to download the file using Safari
+applescript_command='tell application "Safari" to download URL "$url" to file "$download_location$zip_file_name"'
+
